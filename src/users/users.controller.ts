5fedd783240ca371024f4c7f5a1f96f2df375548
erduval <erle.duval@yahoo.com>
2025-07-14 14:11:44 +0200

    Add the service method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,33 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(private readonly userService: UsersService) { }
+
     @Get()
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return role ? role : []
-    }
-
-    @Get('interns')
-    findAllInterns() {
-        return []
+        return this.userService.findAll(role)
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return { id }
+        return this.userService.findOne(+id)
     }
 
     @Post()
-    create(@Body() user: {}) {
-        return user
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.userService.create(user)
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate }
+    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.userService.update(+id, userUpdate)
     }
 
     @Delete(':id')
     deleteOne(@Param('id') id: string) {
-        return { id }
+        return this.userService.delete(+id)
     }
 }
